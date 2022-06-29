982304fdb5c32b0471531fce18928423caf7056b
girlcoder3 <lo14ubna02@gmail.com>
2022-06-29 12:32:31 +0100

    v

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,2 +1,12 @@
-const a = document.getElementById('hello')
-console.log("helloooooooo")
+const a = document.getElementById('1')
+console.log(a)
+
+const button = document.querySelector('.submit');
+console.log("hello")
+const order = document.getElementsByClassName('order').value;
+
+console.log(order);
+
+button.addEventListener("click", function(){
+	console.log(order);
+})
